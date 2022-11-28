27d68a07aba3cce18dad715d58e25988d06b1f11
HugoChaves86 <hugo.chaves@ufba.br>
2022-11-27 22:20:40 -0300

    Ajustes
    Ajustes

--- src/trabalhofinalengsoft/Programa.java
+++ src/trabalhofinalengsoft/Programa.java
@@ -21,19 +21,22 @@ public class Programa {
             System.out.println("\n============================");
             System.out.println("======== Biblioteca ========");
             System.out.println("============================\n");
-            System.out.println("========= Operações ========\n");
+            System.out.println("======== Operações ========");
+            System.out.println("===========================\n");
             System.out.println("Digite 'emp códigoDoUsuario códigoDoLivro' para realizar empréstimo;");
             System.out.println("Digite 'dev códigoDoUsuario códigoDoLivro' para devolver um exemplar;");
             System.out.println("Digite 'res códigoDoUsuario códigoDoLivro' para reservar um exemplar;");
             System.out.println("Digite 'obs códigoDoUsuario códigoDoLivro' para registrar um professor observador;");
             System.out.println("Digite 'rem códigoDoUsuario códigoDoLivro' para remover um professor observador;");
             
-            System.out.println("\n========= Consultas requeridas ========\n");
+            System.out.println("\n========= Consultas requeridas =========");
+            System.out.println("========================================\n");
             System.out.println("Digite 'liv códigoDoLivro' para pesquisar livros por código;");
             System.out.println("Digite 'usu códigoDoUsuario' para pesquisar histórico do usuário;");
             System.out.println("Digite 'ntf códigoDoUsuario' para notificações do professor;");
             
-            System.out.println("\n========= Consultas adicionais ========\n");
+            System.out.println("\n========= Consultas adicionais =========");
+            System.out.println("========================================\n");
             System.out.println("Digite 'exemplares' para listar exemplares;");
             System.out.println("Digite 'usuarios' para listar usuários;");
             System.out.println("Digite 'emprestimos' para listar empréstimos;");
